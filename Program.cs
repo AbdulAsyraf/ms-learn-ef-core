5804d720401bf62d8309d8e2b5e9cecff77ea518
DESKTOP-SNU50BA\abdul <asyraf.abdul@mysoftinn.com>
2022-10-10 18:34:27 +0800

    Migrated from inMemory database to MS SQL Server. Also added delete update
    Changed the DbContext declaration in the code to MS SQL server
    Used the DotNet migration tool to create new database is sql server
    Added the DELETE and PUT paths

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PizzaStore.Models;
+using System.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+
+//SqlConnection conn = new SqlConnection(
+//    new SqlConnectionStringBuilder()
+//    {
+//        DataSource = "127.0.0.1,1433",
+//        InitialCatalog = "DatabaseName",
+//        UserID = "SA",
+//        Password = "Thinkpad_123"
+//    }.ConnectionString
+//);
+
+builder.Services.AddSqlServer<PizzaDb>(new SqlConnectionStringBuilder()
+{
+    DataSource = "127.0.0.1,1433",
+    InitialCatalog = "DatabaseName",
+    UserID = "SA",
+    Password = "Thinkpad_123"
+}.ConnectionString);
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -31,6 +51,30 @@ app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
     await db.Pizzas.AddAsync(pizza);
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null)
+    {
+        return Results.NotFound();
+    }
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
 });
 
 app.Run();
